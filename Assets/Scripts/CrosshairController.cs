173febe624851f17d1a8006b1233ce00ecd18c98
Luke <luke.lukejoe@gmail.com>
2022-10-27 10:39:22 -0700

    Added dynamic crosshair showing player actions

---
+++ Assets/Scripts/CrosshairController.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CrosshairController : MonoBehaviour
+{
+    [SerializeField] private RectTransform crosshair;
+
+    public float currentSize = 30f;
+    public float idleSize = 30f;
+    public float actionSize = 20f;
+    public float jumpSize = 30f;
+    public float speed = 15f;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Fire1"))
+        {
+            currentSize = Mathf.Lerp(currentSize, actionSize, Time.deltaTime * speed);
+        }
+        else
+        {
+            currentSize = Mathf.Lerp(currentSize, idleSize, Time.deltaTime * speed);
+        }
+
+        crosshair.sizeDelta = new Vector2(currentSize, currentSize);
+    }
+}
