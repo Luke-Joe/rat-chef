d94f4fe4d96a65ecf8ce794b78f4c01f9036f4ea
Luke <luke.lukejoe@gmail.com>
2022-11-01 16:48:57 -0700

    Added serializable ingredient class

---
+++ Assets/Scripts/Ingredient.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class Ingredient
+{
+    public string name;
+    public bool isCore;
+    public int quantity;
+    public int status;
+    public int value;
+}
