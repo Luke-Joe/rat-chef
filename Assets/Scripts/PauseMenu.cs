6ca45c76abf72f5d4060ef399619eda6fdcc71a2
emilyz18 <emilyz18@student.ubc.ca>
2023-02-16 18:43:12 -0800

    added menu

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using System;
+
+public class PauseMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public static bool GameIsPaused = false;
+    public GameObject pauseMenuUI;
+
+    // Update is called once per frame
+    public void Update()
+    {
+
+        // int activeScene = SceneManager.GetActiveScene().buildIndex;
+        // Debug.Log(activeScene);
+        // if (activeScene != 0) {
+
+        if (Input.GetKeyDown(KeyCode.Escape)) {
+                    Debug.Log("escape pressed");
+
+            if (GameIsPaused) {
+                Resume();
+            } else {
+                Pause();
+            }
+        }
+        // }
+    }
+
+    void Resume() {
+        Debug.Log("resumed");
+
+        pauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+    }
+
+    void Pause() {
+
+        pauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+        Debug.Log("paused");
+
+    }
+    
+}
