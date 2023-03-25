02e664f18571390e0dd2b3f27765b92e78a8f535
Luke <luke.lukejoe@gmail.com>
2023-03-25 16:21:14 -0700

    Fixed some rats

--- Assets/Scripts/Rat/RatController.cs
+++ Assets/Scripts/Rat/RatController.cs
@@ -9,8 +9,8 @@ public class RatController : MonoBehaviour
     public Transform food;
     [SerializeField] private float rotationSpeed;
     [SerializeField] private Transform ratMouth;
-    [SerializeField] private Transform home;
-    [SerializeField] private Transform despawnZone;
+    public Transform home;
+    public Transform despawnZone;
 
     private bool hasFood;
     private bool isCompleted;
@@ -38,6 +38,8 @@ public class RatController : MonoBehaviour
     void Start()
     {
         agent = GetComponent<NavMeshAgent>();
+        hasFood = false;
+        currPatrolTime = patrolTime;
     }
 
     void Update()
@@ -109,7 +111,14 @@ public class RatController : MonoBehaviour
         {
             if (hitCollider.gameObject.GetComponent<IngredientHandler>() != null && hitCollider.transform.position.y < vertRange)
             {
-                food = hitCollider.gameObject.transform;
+                if (hitCollider.gameObject.GetComponentInParent<RatController>() == null && Vector3.Distance(hitCollider.transform.position, home.position) > 3f)
+                {
+                    food = hitCollider.gameObject.transform;
+                }
+                else
+                {
+                    food = null;
+                }
             }
         }
     }
@@ -123,10 +132,12 @@ public class RatController : MonoBehaviour
             if (RandomPoint(transform.position, range, out point))
             {
                 Debug.DrawRay(point, Vector3.up, Color.blue, 1.0f);
+                movePosition = point;
             }
         }
 
         currPatrolTime -= Time.deltaTime;
+        // Debug.Log(agent.remainingDistance);
     }
 
     void CompletionCheck()
@@ -140,7 +151,6 @@ public class RatController : MonoBehaviour
 
     void DespawnCheck()
     {
-        Debug.Log(Vector3.Distance(gameObject.transform.position, despawnZone.position));
         if (Vector3.Distance(gameObject.transform.position, despawnZone.position) < 0.5f)
         {
             Destroy(gameObject);
@@ -183,27 +193,40 @@ public class RatController : MonoBehaviour
 
     void GrabFood(GameObject food)
     {
-        hasFood = true;
-        heldObject = food;
-        Rigidbody foodRb = food.GetComponent<Rigidbody>();
-        foodRb.useGravity = false;
-        food.transform.SetParent(ratMouth);
+        if (food.GetComponentInParent<RatController>() == null && Vector3.Distance(food.transform.position, home.position) > 3f)
+        {
+            hasFood = true;
+            heldObject = food;
+            Rigidbody foodRb = food.GetComponent<Rigidbody>();
+            foodRb.useGravity = false;
+            food.transform.SetParent(ratMouth);
+        }
+        else
+        {
+            food.transform.SetParent(null);
+        }
     }
 
     void MoveFood()
     {
-        heldObject.transform.position = ratMouth.transform.position;
-        heldObject.transform.rotation = ratMouth.transform.rotation;
+        if (hasFood)
+        {
+            heldObject.transform.position = ratMouth.transform.position;
+            heldObject.transform.rotation = ratMouth.transform.rotation;
+        }
     }
 
     public void DropFood()
     {
-        hasFood = false;
-        Rigidbody foodRb = heldObject.GetComponent<Rigidbody>();
-        foodRb.useGravity = true;
-        foodRb.isKinematic = false;
-        foodRb.AddForce(transform.forward * ratThrowForce, ForceMode.Impulse);
-        heldObject.transform.SetParent(null);
-        heldObject = null;
+        if (hasFood)
+        {
+            hasFood = false;
+            Rigidbody foodRb = heldObject.GetComponent<Rigidbody>();
+            foodRb.useGravity = true;
+            foodRb.isKinematic = false;
+            foodRb.AddForce(transform.forward * ratThrowForce, ForceMode.Impulse);
+            heldObject.transform.SetParent(null);
+            heldObject = null;
+        }
     }
 }
--- Assets/Scripts/Trash.cs
+++ Assets/Scripts/Trash.cs
@@ -1,14 +1,17 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
 
 public class Trash : MonoBehaviour
 {
     private BoxCollider zone;
+    private float ratDespawn;
 
     void Start()
     {
         zone = this.GetComponent<BoxCollider>();
+        ratDespawn = 10f;
     }
 
     void OnTriggerStay(Collider other)
@@ -28,8 +31,27 @@ public class Trash : MonoBehaviour
         }
     }
 
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.GetComponent<RatController>() != null)
+        {
+            other.GetComponent<NavMeshAgent>().enabled = false;
+            other.GetComponent<RatController>().enabled = false;
+            StartCoroutine(RatDespawn(other.gameObject));
+        }
+    }
+
     void OnTriggerExit(Collider other)
     {
-        other.GetComponent<IngredientHandler>().despawnTime = other.GetComponent<IngredientHandler>().originalDespawn;
+        if (other.GetComponent<IngredientHandler>() != null)
+        {
+            other.GetComponent<IngredientHandler>().despawnTime = other.GetComponent<IngredientHandler>().originalDespawn;
+        }
+    }
+
+    private IEnumerator RatDespawn(GameObject rat)
+    {
+        yield return new WaitForSeconds(ratDespawn);
+        Destroy(rat);
     }
 }
